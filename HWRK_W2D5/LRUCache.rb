9d692bf8301b4fd33260740546212960b793d7ae
Omayemi <grend500@gmail.com>
2017-07-13 19:42:50 -0400

    Implement an LRU Cache

--- HWRK_W2D5/LRUCache.rb
+++ HWRK_W2D5/LRUCache.rb
@@ -1,20 +1,24 @@
 class LRUCache
-    def initialize
+    def initialize(size)
+      @max_size = size
+      @cache = []
     end
 
     def count
-      # returns number of elements currently in cache
+      @cache.length
     end
 
     def add(el)
-      # adds element to cache according to LRU principle
+      @cache.delete(el)
+      @cache.shift if count == @max_size
+      @cache << el
     end
 
     def show
-      # shows the items in the cache, with the LRU item first
+      print @cache
     end
 
     private
     # helper methods go here!
 
-  end
+end
